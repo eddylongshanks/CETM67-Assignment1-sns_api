5610486003986743ab5e073e851b6cf6e3e578c6
Christopher Holmes <christopher.holmes@newcastle.co.uk>
2021-07-02 18:47:48 +0100

    Added requirements and Requests script

--- sns_requests.py
+++ sns_requests.py
@@ -4,24 +4,15 @@
 import json
 from requests import post, get
 
-BASE_URL = "http://127.0.0.1:5000"
-#BASE_URL = "http://ec2-54-175-80-131.compute-1.amazonaws.com"
-
-# data = '{"id":"dfjdkf734dsc87","first_name":"sns_api1","last_name":"Stevenson","telephone_number":"07777456278","email_address":"test@holmescentral.co.uk"}'
-# data3 = {"id":"reh56fjr743478","first_name":"sns_api3","email_address":"test@holmescentral.co.uk"}
-# data4 = "{'id':'rehekjr743478','first_name':'sns_api4','email_address':'test@holmescentral.co.uk'}
+BASE_URL = "https://fm6o6wnr3l.execute-api.eu-west-2.amazonaws.com/dev"
+API_KEY = "G5MWtixDKN3xgHcJkWqfc6tTa1a3ZBIv5TE2dVKZ"
+API_HEADERS = {"x-api-key":API_KEY}
 
 data = {
 	"first_name": "vs_SNS_API1",
 	"email_address": "test@holmescentral.co.uk"
 }
 
-
-
-# response = get(BASE_URL + "/get-topics")
-# print(response.text)
-
-response = post(BASE_URL + "/send", json = data)
-print(response.text)
-
-
+response = post(BASE_URL + "/send", headers=API_HEADERS, json = data)
+print("POST New Enquiry... (/send)")
+print("Response: " + response.text)
